725c19878151ddb036878e961917ed600a008654
GabrielPaz00 <gabriel.paz.190@gmail.com>
2023-05-15 22:28:44 -0600

    agregar logo y pantalla de asignaturas

--- sub.js
+++ sub.js
@@ -1,45 +1,8 @@
-var mensaje = document.getElementById("mensaje");
-var contenido1 = document.getElementById("contenido-integral");
-var contenido2 = document.getElementById("contenido-diferencial");
-var contenido3 = document.getElementById("contenido-trigo");
-var contenido4 = document.getElementById("contenido-analitica");
+var mensaje = document.getElementById("menssage");
+var contenido1 = document.getElementById("content-1");
+var contenido2 = document.getElementById("content-2");
+var contenido3 = document.getElementById("content-3");
+var contenido4 = document.getElementById("content-4");
 function mostrar(n) {
-    switch (n) {
-        case 1:
-            contenido2.style.display = "none";
-            contenido3.style.display = "none";
-            contenido4.style.display = "none";
-            mensaje.style.display = "none";
-            contenido1.style.display = "block";
-            break;
-        case 2:
-            contenido1.style.display = "none";
-            contenido3.style.display = "none";
-            contenido4.style.display = "none";
-            mensaje.style.display = "block";
-            mensaje.innerHTML = "";
-            mensaje.innerHTML = "Lo sentimos, aún no contamos con contenido para la asignatura de 'Calculo Diferencial' ):";
-            break;
-        case 3:
-            contenido1.style.display = "none";
-            contenido2.style.display = "none";
-            contenido4.style.display = "none";
-            mensaje.style.display = "block";
-            mensaje.innerHTML = "";
-            mensaje.innerHTML = "Lo sentimos, aún no contamos con contenido para la asignatura de 'Trigonometria' ):";
-            break;
-        case 4:
-            contenido2.style.display = "none";
-            contenido3.style.display = "none";
-            contenido1.style.display = "none";
-            mensaje.style.display = "block";
-            mensaje.innerHTML = "";
-            mensaje.innerHTML = "Lo sentimos, aún no contamos con contenido para la asignatura de 'Geometria Analitica' ):";
-            break;
 
-        default:
-            mensaje.innerHTML = "";
-            mensaje.innerHTML = "Presiona un botón para visualizar la información :)";
-            break;
-    }
 }
